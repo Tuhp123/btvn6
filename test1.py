f3ab50e32bbc34f13716ce18ec7ec8350a3941fd
Tuhp123 <149893630+Tuhp123@users.noreply.github.com>
2024-03-24 17:06:12 +0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
